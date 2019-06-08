f6fe467224f71bc3e25d1e4f74ef9beae75229ef
micsar <micaela.c.saravia@gmail.com>
2019-06-08 10:51:47 -0300

    Mis notas

---
+++ Clase 03-04 notas/js/index.js
@@ -0,0 +1,28 @@
+// Parte 1 
+var nombre = prompt()
+console.log('Hola bienvenido: ' + nombre)
+
+
+// Parte 2 
+
+var pi = 3
+var numeroASumar = prompt()
+numeroASumar = parseFloat(numeroASumar)
+console.log(pi + numeroASumar)
+
+
+// Parte 3
+
+var numeroARestar = prompt()
+numeroARestar = parseFloat(numeroARestar)
+var numeroOriginal = 15
+console.log(numeroOriginal - numeroARestar)
+
+// Parte 4
+
+var numeroUno = prompt()
+var numeroDos = prompt()
+numeroUno = parseFloat(numeroUno)
+numeroDos = parseFloat(numeroDos)
+console.log(numeroUno + numeroDos)
+
