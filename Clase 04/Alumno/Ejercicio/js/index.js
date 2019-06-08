f60288a73c622c336689e598b0129002a76a5a1a
micsar <micaela.c.saravia@gmail.com>
2019-06-08 16:23:45 -0300

    Correccion ejercicio

--- Clase 04/Alumno/Ejercicio/js/index.js
+++ Clase 04/Alumno/Ejercicio/js/index.js
@@ -2,10 +2,10 @@ var examResults = [7, 5, 6, 4, 3, 2, 8]
 
 function promedio(arrayDeNumeros) {
   var acumulador = 0;
-  for (var i = 0; i < arrayDeNumeros.lenght; i += 1) {
+  for (var i = 0; i < arrayDeNumeros.length; i += 1) {
     acumulador = acumulador + arrayDeNumeros[i];
   }
-  return acumulador / arrayDeNumeros.lenght
+  return acumulador / arrayDeNumeros.length
 }
 console.log(examResults)
 console.log(promedio(examResults))
