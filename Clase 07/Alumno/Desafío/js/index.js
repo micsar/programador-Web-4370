c9135d98965958c7bf1a48d444dc4a23123e1b29
micsar <micaela.c.saravia@gmail.com>
2019-06-22 14:29:38 -0300

    Desafio clase 7

--- Clase 07/Alumno/Desafío/js/index.js
+++ Clase 07/Alumno/Desafío/js/index.js
@@ -1,3 +1,21 @@
-var car = '{"price":3500,"model":"Etios","brand":"Toyota"}'
-car = JSON.parse(car)
-console.log(car.price + ' ' + car.model)
+function getLocalList(key) {
+  var nombreKey = typeof key === 'string'
+  if (!nombreKey) {
+    return false
+  }
+  var value = localStorage.getItem(key)
+  // value es null, if(null) no ejecuta, if(!null) ejecuta
+  // value es 'ssdf' if('dfdf')
+  if (!value) { // value === null
+    return []
+  }
+  var parseArray = JSON.parse(value)
+  Array.isArray(parseArray)
+  if (Array.isArray(parseArray)) {
+    return parseArray
+  }
+  return []
+}
+
+localStorage.setItem('Numeros', "[1,2,3]")
+console.log(getLocalList('Numeros'))
